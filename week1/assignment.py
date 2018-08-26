c650078bcf4fac1b3440004e158a4107c0aa5243
pkandathil <prashant@techsamurais.com>
2018-08-26 12:27:07 -0700

    Progress so far on the week1 assignment

---
+++ week1/assignment.py
@@ -0,0 +1,196 @@
+import sys
+sys.path.append("..")
+from common.download_utils import download_week1_resources
+
+# download_week1_resources()
+
+from grader import Grader
+grader = Grader()
+
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+
+from ast import literal_eval
+import pandas as pd
+import numpy as np
+
+def read_data(filename):
+    data = pd.read_csv(filename, sep='\t')
+    data['tags'] = data['tags'].apply(literal_eval)
+    return data
+
+train = read_data('data/train.tsv')
+validation = read_data('data/validation.tsv')
+test = pd.read_csv('data/test.tsv', sep='\t')
+
+X_train, y_train = train['title'].values, train['tags'].values
+X_val, y_val = validation['title'].values, validation['tags'].values
+X_test = test['title'].values
+
+import re
+
+
+REPLACE_BY_SPACE_REREPLACE  = re.compile('[/(){}\[\]\|@,;]')
+BAD_SYMBOLS_RE = re.compile('[^0-9a-z #+_]')
+STOPWORDS = set(stopwords.words('english'))
+
+def text_prepare(text):
+    """
+        text: a string
+        
+        return: modified initial string
+    """
+    text = text.lower()
+    text = re.sub(REPLACE_BY_SPACE_REREPLACE, ' ', text) # replace REPLACE_BY_SPACE_RE symbols by space in text
+    text = re.sub(BAD_SYMBOLS_RE, '', text)# delete symbols which are in BAD_SYMBOLS_RE from text
+    
+    # delete stopwords from text
+    textWords = text.split()
+    resultwords  = [word for word in textWords if word.lower() not in STOPWORDS]
+    text = ' '.join(resultwords)
+    return text
+
+def test_text_prepare():
+    examples = ["SQL Server - any equivalent of Excel's CHOOSE function?",
+                "How to free c++ memory vector<int> * arr?"]
+    answers = ["sql server equivalent excels choose function", 
+               "free c++ memory vectorint arr"]
+    for ex, ans in zip(examples, answers):
+        if text_prepare(ex) != ans:
+            return "Wrong answer for the case: '%s'" % ex
+    return 'Basic tests are passed.'
+
+prepared_questions = []
+for line in open('data/text_prepare_tests.tsv', encoding='utf-8'):
+    line = text_prepare(line.strip())
+    prepared_questions.append(line)
+text_prepare_results = '\n'.join(prepared_questions)
+grader.submit_tag('TextPrepare', text_prepare_results)
+
+X_train = [text_prepare(x) for x in X_train]
+X_val = [text_prepare(x) for x in X_val]
+X_test = [text_prepare(x) for x in X_test]
+
+# print('-----------------------')
+# print(X_train[:3])
+# print('-----------------------')
+
+# Dictionary of all tags from train corpus with their counts.
+tags_counts = {}
+# Dictionary of all words from train corpus with their counts.
+words_counts = {}
+
+def count_words(word_list):
+  result = {}
+  for line in word_list:
+    if isinstance(line, list):
+      line = line[0]
+    words = line.split( )
+    for word in words:
+      if(not word in result):
+        result[word] = 0
+      result[word] += 1
+  return result
+
+tags_counts = count_words(y_train)
+words_counts = count_words(X_train)
+
+most_common_tags = sorted(tags_counts.items(), key=lambda x: x[1], reverse=True)[:3]
+most_common_words = sorted(words_counts.items(), key=lambda x: x[1], reverse=True)[:3]
+
+grader.submit_tag('WordsTagsCount', '%s\n%s' % (','.join(tag for tag, _ in most_common_tags), 
+                                                ','.join(word for word, _ in most_common_words)))
+
+##########################################################
+
+
+
+DICT_SIZE = 5000
+INDEX_TO_WORDS = {}
+index = 0
+for word in words_counts:
+  INDEX_TO_WORDS[index] = word
+  index += 1
+
+index = 0
+WORDS_TO_INDEX = {}
+for word in words_counts:
+  WORDS_TO_INDEX[word] = index
+  index += 1
+
+ALL_WORDS = WORDS_TO_INDEX.keys()
+
+def my_bag_of_words(text, words_to_index, dict_size):
+    """
+        text: a string
+        dict_size: size of the dictionary
+        
+        return a vector which is a bag-of-words representation of 'text'
+    """
+    result_vector = np.zeros(dict_size)
+    words = text.split( )
+    for word in words:
+      if(word in words_to_index):
+        index = words_to_index[word]
+        if (index >= dict_size):
+          continue
+        result_vector[index] = 1
+    return result_vector
+
+def test_my_bag_of_words():
+    words_to_index = {'hi': 0, 'you': 1, 'me': 2, 'are': 3}
+    examples = ['hi how are you']
+    answers = [[1, 1, 0, 1]]
+    for ex, ans in zip(examples, answers):
+        if (my_bag_of_words(ex, words_to_index, 4) != ans).any():
+            return "Wrong answer for the case: '%s'" % ex
+    return 'Basic tests are passed.'
+
+print(test_my_bag_of_words())
+
+########################################################
+from scipy import sparse as sp_sparse
+
+X_train_mybag = sp_sparse.vstack([sp_sparse.csr_matrix(my_bag_of_words(text, WORDS_TO_INDEX, DICT_SIZE)) for text in X_train])
+X_val_mybag = sp_sparse.vstack([sp_sparse.csr_matrix(my_bag_of_words(text, WORDS_TO_INDEX, DICT_SIZE)) for text in X_val])
+X_test_mybag = sp_sparse.vstack([sp_sparse.csr_matrix(my_bag_of_words(text, WORDS_TO_INDEX, DICT_SIZE)) for text in X_test])
+print('X_train shape ', X_train_mybag.shape)
+print('X_val shape ', X_val_mybag.shape)
+print('X_test shape ', X_test_mybag.shape)
+
+#########################################################
+
+row = X_train_mybag[10].toarray()[0]
+non_zero_elements_count = 0
+for value in row:
+  if (value == 0):
+    non_zero_elements_count += 1
+print('----------------------')
+print(non_zero_elements_count)
+print('----------------------')
+grader.submit_tag('BagOfWords', str(non_zero_elements_count))
+
+##############################################################
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+def tfidf_features(X_train, X_val, X_test):
+    """
+        X_train, X_val, X_test — samples        
+        return TF-IDF vectorized representation of each sample and vocabulary
+    """
+    # Create TF-IDF vectorizer with a proper parameters choice
+    # Fit the vectorizer on the train set
+    # Transform the train, test, and val sets and return the result
+    
+
+    tfidf_vectorizer = TfidfVectorizer (min_df=0.001, max_df=0.9, ngram_range=(1,2), token_pattern= '(\S+)')
+    X_train = tfidf_vectorizer.fit_transform(X_train)
+    X_val = tfidf_vectorizer.fit_transform(X_val)
+    X_test = tfidf_vectorizer.fit_transform(X_test)
+    
+    return X_train, X_val, X_test, tfidf_vectorizer.vocabulary_
+
+X_train_tfidf, X_val_tfidf, X_test_tfidf, tfidf_vocab = tfidf_features(X_train, X_val, X_test)
+tfidf_reversed_vocab = {i:word for word,i in tfidf_vocab.items()}
+
